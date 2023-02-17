412a0f48af6d846fd9327a261d9eaf109148965e
Frah01 <vinario.francesco@gmail.com>
2023-02-17 10:39:35 +0100

    Aggiunto stile e ciclo per icone

--- resources/views/films.blade.php
+++ resources/views/films.blade.php
@@ -22,6 +22,30 @@
                 @endforeach
             </div>
         </div>
+        <div class="row">
+            <div class="col 12">
+                <div class="d-flex justify-content-center pb-5">
+                    <button class="film-button text-uppercase text-white" >Load More</button>
+                </div>
+            </div>
+        </div>
+        {{-- <div class="container">
+            <div class="row">
+                <div class="col">
+                    <ul class="d-flex list-unstyled justify-content-between py-5 text-light">
+                        @foreach ($icons as $icon)
+                        <li>
+                            <div>
+                                <img src="{{asset(Vite::asset($icon['img']))}}" alt="{{$icon['nome']}}">
+                            </div>
+                            <p>{{$icon['nome']}}</p>
+                        </li>
+                            
+                        @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div> --}}
     </div>
     @include('partials.footer')
 </body>
